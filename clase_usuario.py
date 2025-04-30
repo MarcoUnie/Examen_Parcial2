0743a9e8821dc85ca019dc01d65932d0f6a77c45
MarcoUnie <mmagecoyt@gmail.com>
2025-04-30 10:19:35 +0200

    clase usuario

---
+++ clase_usuario.py
@@ -0,0 +1,26 @@
+from ejercicio_4.clase_libro_genero import generolibro
+from ejercicio_4.clase_libro import Book
+class User:
+    def __init__(self, name):
+        self._name = name
+        self._borrow_history = []  # Historial de libros prestados
+
+    def get_name(self):
+        return self._name
+
+    def get_history(self):
+        return self._borrow_history
+
+    def borrow_book(self, book: Book):
+        if book.borrow():
+            self._borrow_history.append(book)
+            print(f"{self._name} ha tomado prestado: {book.get_title()}")
+        else:
+            print(f"{book.get_title()} no está disponible para préstamo.")
+
+    def return_book(self, book: Book):
+        if book in self._borrow_history:
+            book.return_book()
+            print(f"{self._name} ha devuelto: {book.get_title()}")
+        else:
+            print(f"{self._name} no tiene ese libro.")
